a073247088b6e2932f94b88439d25bc30c79c41b
dcortez0817 <darien.cortez@gmail.com>
2016-12-09 15:13:48 -0600

    Update main.py

--- Assignments/prog_3/main.py
+++ Assignments/prog_3/main.py
@@ -1,5 +1,46 @@
 import imageEdit
 from PIL import Image
-import urllib, cStringIO
-import random
+import sys
 
+
+if __name__ == "__main__":
+	
+	filename = input("jagger.jpg")
+	
+	try:
+		img = Image.open(filename)
+	except:
+		print("File does not exist")
+		sys.exit()
+	
+	img2 = img.copy()
+	img3 = img.copy()
+	img4 = img.copy()
+	img5 = img.copy()
+	img6 = img.copy()
+	
+	img2.save('copy.jpg')
+	
+	img = imageEdit.blur(img)
+	
+	img.save('blur.jpg')
+	
+	img2 = imageEdit.flip(img2)
+	
+	img2.save('flip.jpg')
+	
+	img3 = imageEdit.glass_effect(img3)
+	
+	img3.save('glass.jpg')
+	
+	img4 = imageEdit.posterize(img4)
+	
+	img4.save('poster.jpg')
+	
+	img5 = imageEdit.warhol(img5)
+	
+	img5.save('warhol.jpg')
+	
+	img6 = imageEdit.solarize(img6)
+	
+	img6.save('solar.jpg')
